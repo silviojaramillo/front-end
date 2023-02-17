3d7d2f23e0ed08eeea1d5ae1cd2b6ea4ee089101
Alex Jaramillo <91851947+silviojaramillo@users.noreply.github.com>
2023-02-16 19:07:46 -0500

    Update

--- js/promise.js
+++ js/promise.js
@@ -9,7 +9,15 @@ let miPromise = new Promise((resolved,rejected) =>{
 });
 
 
-miPromise.then(
-    valor => console.log(valor),
-    error => console.log(error)
-);
+/* miPromise
+.then(valor => console.log(valor))
+.catch(error => console.log(error)); */
+
+let promise = new Promise(resolved =>{
+    console.log('Inicio de la promesa');
+    setTimeout(()=>resolved('Hola desde promesa setTimeout'),3000);
+    console.log('Fin de la promesa');
+});
+
+
+promise.then(valor=>console.log(valor));
